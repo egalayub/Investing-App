7b2c68a13a56eb66f47261ab79b26b51c5e082e0
Ayub <egalayub2@gmail.com>
2023-04-27 17:23:15 -0400

    First Commit

---
+++ src/main/java/com/example/Investingapp/InvestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Investingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Investingapp/InvestingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Investingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
